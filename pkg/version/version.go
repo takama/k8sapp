7da2676d6dfe8f4ef453ceca6d4add7d147ac72d
Igor Dolzhikov <bluesriverz@gmail.com>
2017-09-02 12:21:57 +0700

    Added a simple app that show version

---
+++ pkg/version/version.go
@@ -0,0 +1,14 @@
+// Copyright 2017 Igor Dolzhikov. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package version
+
+var (
+	// RELEASE returns the release version
+	RELEASE = "UNKNOWN"
+	// REPO returns the git repository URL
+	REPO = "UNKNOWN"
+	// COMMIT returns the short sha from git
+	COMMIT = "UNKNOWN"
+)
