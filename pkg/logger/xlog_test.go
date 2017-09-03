04054500c78c1b1a6cdfbcbaf0a62a0a08c8ea87
Igor Dolzhikov <bluesriverz@gmail.com>
2017-09-03 21:22:28 +0700

    Added test for output types

--- pkg/logger/xlog_test.go
+++ pkg/logger/xlog_test.go
@@ -1,12 +1,23 @@
 package logger
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewXLog(t *testing.T) {
-	log := newXLog(&Config{
+	log1 := newXLog(&Config{
 		Level: LevelDebug,
 	})
-	if log == nil {
+	if log1 == nil {
+		t.Error("Got uninitialized XLog logger")
+	}
+	log2 := newXLog(&Config{
+		Level: LevelInfo,
+		Out:   os.Stdout,
+		Err:   os.Stdout,
+	})
+	if log2 == nil {
 		t.Error("Got uninitialized XLog logger")
 	}
 }
