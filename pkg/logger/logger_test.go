685cab76b4e83e39fa5b8e993b2eb9305aa59a85
Igor Dolzhikov <bluesriverz@gmail.com>
2017-09-05 19:04:51 +0700

    improve testing coverage

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -2,10 +2,19 @@ package logger
 
 import "testing"
 
+const (
+	customLevel       Level = 17
+	customLevelString       = "17"
+)
+
 func TestLevelString(t *testing.T) {
 	for _, l := range []Level{LevelDebug, LevelInfo, LevelWarn, LevelError, LevelFatal} {
 		if len(l.String()) <= 1 {
 			t.Errorf("Got empty string for %v log level", l)
 		}
+	}
+	if customLevel.String() != customLevelString {
+		t.Error("All undefined levels should be presented as string")
+		t.Errorf("invalid log level:\ngot:  %s\nwant: %s", customLevel.String(), customLevelString)
 	}
 }
