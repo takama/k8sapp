8c872eb3c9f4c077c66828670003e6551bc1ed7a
Igor Dolzhikov <bluesriverz@gmail.com>
2017-09-12 23:34:59 +0700

    service test

--- pkg/service/service_test.go
+++ pkg/service/service_test.go
@@ -1,10 +1,22 @@
 package service
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/takama/k8sapp/pkg/config"
+)
 
 func TestRun(t *testing.T) {
-	err := Run()
+	cfg := new(config.Config)
+	err := cfg.Load(config.SERVICENAME)
+	if err != nil {
+		t.Error("Expected loading of environment vars, got", err)
+	}
+	router, err := Setup(cfg)
 	if err != nil {
 		t.Errorf("Fail, got '%s', want '%v'", err, nil)
+	}
+	if router == nil {
+		t.Error("Expected new router, got nil")
 	}
 }
