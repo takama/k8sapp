14c347d8e50b1b3377660cae75b00e3ac4a36219
Igor Dolzhikov <bluesriverz@gmail.com>
2017-09-14 00:35:31 +0700

    Added handlers tests

---
+++ pkg/handlers/handler_test.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/takama/k8sapp/pkg/config"
+	"github.com/takama/k8sapp/pkg/logger"
+	"github.com/takama/k8sapp/pkg/logger/standard"
+	"github.com/takama/k8sapp/pkg/router/bitroute"
+	"github.com/takama/k8sapp/pkg/version"
+)
+
+func TestRoot(t *testing.T) {
+	h := New(standard.New(&logger.Config{}), new(config.Config))
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h.Base(h.Root)(bitroute.NewControl(w, r))
+	})
+
+	testHandler(t, handler, http.StatusOK, fmt.Sprintf("%s v%s", config.SERVICENAME, version.RELEASE))
+}
+
+func testHandler(t *testing.T, handler http.HandlerFunc, code int, body string) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Error(err)
+	}
+
+	trw := httptest.NewRecorder()
+	handler.ServeHTTP(trw, req)
+
+	if trw.Code != code {
+		t.Error("Expected status code:", code, "got", trw.Code)
+	}
+	if trw.Body.String() != body {
+		t.Error("Expected body", body, "got", trw.Body.String())
+	}
+}
